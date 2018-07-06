8065a10e2f90bdf5323e7699da017321d36c167a
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-06 22:41:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
   configure do
     enable :sessions
@@ -8,6 +8,12 @@ class App < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+
+  post '/checkout' do
+    binding.pry
+
+    erb:checkout
   end
 
 end
