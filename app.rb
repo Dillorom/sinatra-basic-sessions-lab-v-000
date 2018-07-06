c4018d8fec40fa26bbde0edcc8c91881d0909d82
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-06 22:53:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 require 'pry'
 class App < Sinatra::Base
   configure do
-    enable :Sessions
+    enable :sessions
     set :session_secret, "secret"
   end
 
